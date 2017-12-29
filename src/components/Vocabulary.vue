2bdfccf468a6b1ae5c14e5111e9591acee841cb2
Mugiwara <mugiwaranojem@gmail.com>
2017-12-29 11:40:54 +0800

    init commit

---
+++ src/components/Vocabulary.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="vocabulary">
+		<h1>{{ msg }}</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Vocabulary',
+		data() {
+			return {
+				msg: 'Vocabulary page',
+				words: []
+			}
+		},
+		created () {
+			this.words = this.$words
+		}
+	}
+</script>
